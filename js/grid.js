25ddae245e9c90ac807f3317f0268a83efab5014
Jordan Orelli <jordanorelli@gmail.com>
2012-03-04 18:03:25 -0500

    raphael's drawing interface is nicer.

--- js/grid.js
+++ js/grid.js
@@ -1,43 +1,16 @@
-var Grid = (function($) {
-  var canvas = null;
-  var context = null;
-  var tileWidth = null;
-  var tileHeight = null;
-  var tiles = [];
-
-  var draw = function(options) {
-    var colWidth;
-    var rowHeight;
-
-    this.canvas = document.getElementById(options.id);
-    this.context = this.canvas.getContext("2d");
-
-    this.tileWidth = (this.canvas.width - (options.colGutter * (1 + options.columns))) / options.columns;
-    this.tileHeight = (this.canvas.height - (options.rowGutter * (1 + options.rows))) / options.rows;
-
-    colWidth = this.canvas.width / options.columns;
-    rowHeight = this.canvas.height / options.rows;
-
-    this.context.moveTo(0.5, 0);
-    this.context.lineTo(0.5, this.canvas.height);
-
-    for(var x = 0.5; x <= this.canvas.width + 0.5; x += colWidth) {
-      this.context.moveTo(x, 0);
-      this.context.lineTo(x, this.canvas.height + 0.5);
+function PuzzleGrid(container_id, width) {
+  this.width = width;
+  this.paper = new Raphael(container_id, width, width);
+  this.tileGutter = 4; // spacing between tiles
+  this.tileWidth = Math.floor((this.width - 5 * this.tileGutter) / 4);
+  for(var x = this.tileGutter; x < this.width; x += this.tileWidth + this.tileGutter) {
+    for(var y = this.tileGutter; y < this.width; y += this.tileWidth + this.tileGutter) {
+      this.paper.rect(x, y, this.tileWidth, this.tileWidth);
     }
-
-    for(var y = 0.5; y <= this.canvas.height + 0.5; y += rowHeight) {
-      this.context.moveTo(0, y);
-      this.context.lineTo(this.canvas.width + 0.5, y);
-    }
-
-    this.context.strokeStyle = "#aaa";
-    this.context.stroke();
-  };
-
-  return {
-    draw: draw,
-    canvas: canvas,
-    context: context
   }
-})(window.jQuery);
+}
+
+// when invoked, tells us if our puzzlegrid is solved or not.
+PuzzleGrid.prototype.solved = function() {
+  return false;
+};
