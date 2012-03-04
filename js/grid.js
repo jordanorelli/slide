30d84e7f63348c286682a49c6cc1288f988a9c4b
Jordan Orelli <jordanorelli@gmail.com>
2012-03-04 16:35:38 -0500

    hmm.  maybe i should try rafael.

--- js/grid.js
+++ js/grid.js
@@ -1,19 +1,36 @@
 var Grid = (function($) {
   var canvas = null;
   var context = null;
+  var tileWidth = null;
+  var tileHeight = null;
+  var tiles = [];
 
-  var draw = function(canvasId, density) {
-    this.canvas = document.getElementById(canvasId);
+  var draw = function(options) {
+    var colWidth;
+    var rowHeight;
+
+    this.canvas = document.getElementById(options.id);
     this.context = this.canvas.getContext("2d");
-    console.log(this.canvas.width);
-    for(var x = 0; x <= this.canvas.width; x += density) {
+
+    this.tileWidth = (this.canvas.width - (options.colGutter * (1 + options.columns))) / options.columns;
+    this.tileHeight = (this.canvas.height - (options.rowGutter * (1 + options.rows))) / options.rows;
+
+    colWidth = this.canvas.width / options.columns;
+    rowHeight = this.canvas.height / options.rows;
+
+    this.context.moveTo(0.5, 0);
+    this.context.lineTo(0.5, this.canvas.height);
+
+    for(var x = 0.5; x <= this.canvas.width + 0.5; x += colWidth) {
       this.context.moveTo(x, 0);
-      this.context.lineTo(x, this.canvas.height);
+      this.context.lineTo(x, this.canvas.height + 0.5);
     }
-    for(var y = 0; y <= this.canvas.height; y += density) {
+
+    for(var y = 0.5; y <= this.canvas.height + 0.5; y += rowHeight) {
       this.context.moveTo(0, y);
-      this.context.lineTo(this.canvas.width, y);
+      this.context.lineTo(this.canvas.width + 0.5, y);
     }
+
     this.context.strokeStyle = "#aaa";
     this.context.stroke();
   };
