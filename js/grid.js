1d2cb1a063573b673ea65a1b7f669dec0eb09ea0
Jordan Orelli <jordanorelli@gmail.com>
2012-03-04 20:30:09 -0500

    single-tile sliding works for clicks now, yay

--- js/grid.js
+++ js/grid.js
@@ -18,27 +18,73 @@ function Tile(grid, column, row) {
     this.attr("fill", "white");
   });
   this.elem.click(function() {
-    this.tile.move_down();
+    if(this.tile.can_move()) {
+      this.tile.slide();
+    };
   });
 }
 
 // can move if in the same row as the empty tile and adjacent column
 // can move if in the same column as the empty tile and adjacent row
 Tile.prototype.can_move = function() {
-  return true;
+  if(this.row === this.grid.empty_row && Math.abs(this.column - this.grid.empty_column) === 1) {
+    return true;
+  } else if (this.column === this.grid.empty_column && Math.abs(this.row - this.grid.empty_row) === 1) {
+    return true;
+  }
+  return false;
 };
 
-Tile.prototype.move_down = function() {
-  this.elem.animate({
-    y: this.elem.attrs.y + this.grid.tileHeight + this.grid.tileGutter
-  }, 200, "ease-in-out");
-  this.row += 1;
+Tile.prototype.slide = function() {
+  if(this.column === this.grid.empty_column) {
+    if(this.row < this.grid.empty_row) {
+      this.move("down");
+    } else {
+      this.move("up");
+    }
+  } else if (this.row === this.grid.empty_row) {
+    if(this.column < this.grid.empty_column) {
+      this.move("right");
+    } else {
+      this.move("left");
+    }
+  }
 };
 
-Tile.prototype.move_up = function () {
-  this.elem.animate({
-    y: this.elem.attrs.y - this.grid.tileHeight - this.grid.tileGutter
-  });
+Tile.prototype.move = function(direction) {
+  var params = {};
+  switch(direction) {
+    case "up":
+      params = {
+        y: this.elem.attrs.y - this.grid.tileHeight - this.grid.tileGutter
+      };
+      this.row--;
+      this.grid.empty_row++;
+      break;
+    case "down":
+      params = {
+        y: this.elem.attrs.y + this.grid.tileHeight + this.grid.tileGutter
+      };
+      this.row++;
+      this.grid.empty_row--;
+      break;
+    case "left":
+      params = {
+        x: this.elem.attrs.x - this.grid.tileWidth - this.grid.tileGutter
+      };
+      this.column--;
+      this.grid.empty_column++;
+      break;
+    case "right":
+      params = {
+        x: this.elem.attrs.x + this.grid.tileWidth + this.grid.tileGutter
+      };
+      this.column++;
+      this.grid.empty_column--;
+      break;
+  }
+  this.elem.animate(params, 200, "ease-in-out");
+  this.grid.log_empty();
 };
 
 function PuzzleGrid(container_id, width, height) {
@@ -48,16 +94,18 @@ function PuzzleGrid(container_id, width, height) {
 
   this.rows = 4;
   this.columns = 4;
-  this.empty_row = 3;
+  this.empty_row = 2;
   this.empty_column = 3;
   this.tileGutter = 4; // spacing between tiles
 
   this.tileWidth = Math.floor((this.width - (1 + this.columns) * this.tileGutter) / this.columns);
   this.tileHeight = Math.floor((this.height - (1 + this.rows) * this.tileGutter) / this.rows);
 
+  this.log_empty();
+
   for(var x = 0; x < this.columns; x++) {
     for(var y = 0; y < this.rows; y++) {
-      if(x == this.empty_row && y == this.empty_column) { continue; }
+      if(x == this.empty_column && y == this.empty_row) { continue; }
       new Tile(this, x, y);
     }
   }
@@ -66,4 +114,11 @@ function PuzzleGrid(container_id, width, height) {
 // when invoked, tells us if our puzzlegrid is solved or not.
 PuzzleGrid.prototype.solved = function() {
   return false;
+};
+
+PuzzleGrid.prototype.log_empty = function() {
+  /*
+  console.log("Empty row: " + this.empty_row);
+  console.log("Empty column: " + this.empty_column);
+  */
 };
